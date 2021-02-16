92274531f75b6ac1a1dbc793af66346a809f48f8
Nikola <nikolapantelic434@gmail.com>
2021-02-16 14:19:06 +0100

    Initial components for user profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/navbar/navbar.component';
+import { SearchComponent } from './core/navbar/search/search.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserComponent } from './profile/user/user.component';
+import { OverviewComponent } from './profile/overview/overview.component';
+import { ReposComponent } from './profile/repos/repos.component';
+import { ContributionsComponent } from './profile/overview/contributions/contributions.component';
+import { RepoComponent } from './profile/overview/repos/repo/repo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    ProfileComponent,
+    UserComponent,
+    OverviewComponent,
+    ReposComponent,
+    ContributionsComponent,
+    RepoComponent
   ],
   imports: [
     BrowserModule
