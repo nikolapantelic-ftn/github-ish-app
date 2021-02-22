3ad1e3cd34d77b65f325d39c0565d70464edf53d
Nikola <nikolapantelic434@gmail.com>
2021-02-22 15:22:40 +0100

    Decoupled navbar search into component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
+import { OverviewComponent } from './profile/overview/overview.component';
+import {ReposComponent} from './profile/repos/repos.component';
 
 const routes: Routes = [
-  { path: '', component: ProfileComponent }
+  { path: '', redirectTo: 'profile', pathMatch: 'full' },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    children: [
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
+      { path: 'overview', component: OverviewComponent },
+      { path: 'repos', component: ReposComponent }
+    ]
+  }
 ];
 
 @NgModule({
